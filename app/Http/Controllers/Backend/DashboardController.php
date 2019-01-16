93a025a6899c18a5fcfd36b382bcb59be52af8ab
Anuj Jaha <er.anujjaha@gmail.com>
2019-01-16 22:49:31 +0530

    Added Global Push notification

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Access\User\User;
 
 /**
  * Class DashboardController.
@@ -15,5 +17,35 @@ class DashboardController extends Controller
     public function index()
     {
         return view('backend.dashboard');
+    }
+
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function sendPushNotifications(Request $request)
+    {
+    	if($request->has('notification_text'))
+    	{
+    		$users 	= User::all();
+    		$text  	= $request->get('notification_text');
+    		$sr 	= 0;
+
+    		foreach($users as $user)
+    		{
+    			$payload  = [
+    			    'mtitle'    => '',
+    			    'mdesc'     => $text,
+    			    'ntype'     => 'GENERAL_NOTIFICATION'
+    			];
+
+    			access()->sentPushNotification($user, $payload);
+
+    			$sr++;
+    		}
+
+    		return redirect()->route('admin.push-notifications')->withFlashSuccess("Total ".$sr." Push Notification Send Successfully!");
+    	}
+
+		return view('backend.push-notification');
     }
 }
--- routes/Backend/Dashboard.php
+++ routes/Backend/Dashboard.php
@@ -4,3 +4,5 @@
  * All route names are prefixed with 'admin.'.
  */
 Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+
+Route::any('push-notifications', 'DashboardController@sendPushNotifications')->name('push-notifications');
